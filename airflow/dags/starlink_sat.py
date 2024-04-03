ca5d193fbb4bccaadcf1c3b9a2ddaece15b10138
ShustGF <olga-permyakova-74@mail.ru>
2024-04-03 21:48:20 +0300

    SCRUM-13 change name variable 'URL'

--- airflow/dags/starlink_sat.py
+++ airflow/dags/starlink_sat.py
@@ -1,20 +1,52 @@
 import requests
 import json
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import Column,Numeric , String, Integer, ForeignKey, Boolean, DateTime, JSON
 
-URL = "https://api.spacexdata.com/v4/starlink"
+db_name = 'replica_logical'
+db_user = 'postgres'
+db_pass = 'gfh0km'
+db_host = 'localhost'
+db_port = '5431'
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
 
-def return_request(URL):
-  r = requests.get(URL)
-  if r.status_code == 404:
+url_starlink = "https://api.spacexdata.com/v4/starlink"
+Base = sqlalchemy.orm.declarative_base()
+
+def return_request(url):
+  req_answer = requests.get(url)
+  if req_answer.status_code == 404:
     raise AttributeError("Неверное значение URL-адреса")
-  return r.text
+  return req_answer.text
 
+def object_starlink(sat_json):
+  starlink_sat = Starlink_sat(
+    spacetrack=sat_json['spaceTrack'], 
+    version=sat_json['version'],
+    launch=sat_json['launch'],
+    longitude=sat_json['longitude'],
+    latitude=sat_json['latitude'],
+    height_km=sat_json['height_km'],
+    velocity_kms=sat_json['velocity_kms'],
+    id=sat_json['id']
+  )
+  return starlink_sat
 
+class Starlink_sat(Base):
+  __tablename__ = 'starlink_satellites'
+  spacetrack = Column(JSON)
+  version = Column(String)
+  launch = Column(String)
+  longitude = Column(Numeric)
+  latitude = Column(Numeric)
+  height_km = Column(Numeric)
+  velocity_kms = Column(Numeric)
+  id = Column(String, primary_key=True)
 
-class Starlink_sat:
-  def __init__(self, spaceTrack, version=None, launch=None, longitude=None, latitude=None, height_km=None, velocity_kms=None, id=None):
-    self.spaceTrack = spaceTrack
+  def __init__(self, spacetrack, version=None, launch=None, longitude=None, latitude=None, height_km=None, velocity_kms=None, id=None):
+    self.spacetrack = spacetrack
     self.version = version
     self.launch = launch
     self.longitude = longitude
@@ -32,22 +64,9 @@ class Starlink_sat:
       self._spaceTrack = json.loads(spaceTrack)
 
 
-
-
-sat_json =  json.loads(return_request(URL))[0]
-
-starlink_sat = Starlink_sat(
-  spaceTrack=sat_json['spaceTrack'], 
-  version=sat_json['version'],
-  launch=sat_json['launch'],
-  longitude=sat_json['longitude'],
-  latitude=sat_json['latitude'],
-  height_km=sat_json['height_km'],
-  velocity_kms=sat_json['velocity_kms'],
-  id=sat_json['id']
-)
-
-print(starlink_sat.spaceTrack)
-
-
-engine = create_engine('postgresql+psycopg2://postgres:gfh0km@localhost:5433/postgres')
+engine = create_engine(db_string)
+session = Session(bind=engine)
+session.add_all([object_starlink(json_sat) for json_sat in json.loads(return_request(url_starlink))])
+# print(session.new)
+session.commit()
+print("Выполнено")
