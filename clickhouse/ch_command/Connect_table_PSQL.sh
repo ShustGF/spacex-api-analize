1f476f3c9e2e3592919cf8c8f1fa8fa6d7875d34
ShustGF <olga-permyakova-74@mail.ru>
2024-05-18 21:09:57 +0300

    SCRUM-196 сделан цикл при создании таблицы

--- clickhouse/ch_command/Connect_table_PSQL.sh
+++ clickhouse/ch_command/Connect_table_PSQL.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-clickhouse-client -q "CREATE TABLE my_database.starlink_satellites ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'starlink_satellites', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
 clickhouse-client -q "CREATE TABLE my_database.launches (
 	fairings String NULL,
 	links String NULL,
@@ -31,19 +30,8 @@ clickhouse-client -q "CREATE TABLE my_database.launches (
 	id String NULL
 )
 ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'launches', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.capsules
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'capsules', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.cores 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'cores', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.crew 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'crew', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.landpads 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'landpads', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.launchpads 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'launchpads', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.payload 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'payload', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.ships 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'ships', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
-clickhouse-client -q "CREATE TABLE my_database.rockets 
-ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', 'rockets', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
+
+for table in starlink_satellites capsules cores crew landpads launchpads payload ships rockets
+do
+clickhouse-client -q "CREATE TABLE my_database.$table ENGINE = PostgreSQL('server_subscription:5432', 'postgres_subscriber', '$table', 'postgres', '$POSTGRES_SUBSCRIPTION_PASSWORD');"
+done
